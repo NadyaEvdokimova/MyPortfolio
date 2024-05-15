b83e3c2855c34260343c884e05d9f0aca2cee42d
NadyaEvdokimova <146958813+NadyaEvdokimova@users.noreply.github.com>
2024-05-15 14:23:23 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,57 @@
+from flask import Flask, render_template, send_file, request
+from flask_bootstrap import Bootstrap5
+import smtplib
+from datetime import datetime
+import os
+
+app = Flask(__name__)
+Bootstrap5(app)
+OWN_EMAIL = os.environ.get('EMAIL')
+OWN_PASSWORD = os.environ.get('PASSWORD')
+
+
+@app.context_processor
+def inject_now():
+    return {'now': datetime.utcnow()}
+
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+
+@app.route('/download')
+def download_cv():
+    path = "Nadja Evdokimova Junior Python Developer.docx"
+    return send_file(path, as_attachment=True)
+
+
+@app.route('/portfolio')
+def portfolio():
+    return render_template('portfolio.html')
+
+
+@app.route('/contact', methods=["GET", "POST"])
+def contact():
+    if request.method == "POST":
+        data = request.form
+        send_email(data["name"], data["email"], data["message"])
+        return render_template("contact.html", msg_sent=True)
+    return render_template("contact.html", msg_sent=False)
+
+
+def send_email(name, email, message):
+    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nMessage:{message}"
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(OWN_EMAIL, OWN_PASSWORD)
+        connection.sendmail(OWN_EMAIL, OWN_EMAIL, email_message)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
